a075090c9fc56cad7675cc638b060a0f301e399e
Daniel Amariles <daniel.amariles88@gmail.com>
2017-08-23 12:11:22 -0500

    avoid reading commented treatments, changing config misspelling, renaming the Enums

--- src/org/ciat/cmit/control/RunConfig.java
+++ src/org/ciat/cmit/control/RunConfig.java
@@ -15,73 +15,66 @@ public class RunConfig {
 
 	public ModelRunManager getModelRunManager(File config) {
 		List<CoefficientDomain> domains = new ArrayList<CoefficientDomain>();
-
 			
+		String modelShortName = App.prop.getProperty("crop.shortname");
+		String fileA = App.prop.getProperty("fileA.location");
+		String fileT = App.prop.getProperty("fileT.location");
+		String fileXHead = App.prop.getProperty("fileX.head.location");
+		String fileXTail = App.prop.getProperty("fileX.tail.location");
+		String culHead = App.prop.getProperty("fileCUL.head.location");
+		String maxFiles = App.prop.getProperty("subfolders.number");
+		String vrname = App.prop.getProperty("CUL.varname");
+		String eco = App.prop.getProperty("CUL.ECO");
+		int n = Integer.parseInt(App.prop.getProperty("variable.number"));
+		String domi = "";
+		for (int i = 0; i < n;) {
+			domi = App.prop.getProperty("variable" + ++i);
+			String param[] = domi.split(" ");
+			domains.add(new CoefficientDomain(Double.parseDouble(param[0]), Double.parseDouble(param[1]), Double.parseDouble(param[2]), new DecimalFormat(param[3]), "%1$" + param[4] + "s"));
+		}
+		CropModel model = new CropModel(modelName, modelShortName, new File(fileA), new File(fileT), new File(fileXHead), new File(fileXTail), new File(culHead));
+		CropModelRun mr = new CropModelRun(domains, model, Integer.parseInt(maxFiles), vrname, eco, getTreatments(new File(fileXHead)));
+		ModelRunManager mrg = new ModelRunManager(mr);
 
-			String modelName = App.prop.getProperty("model.name");
-			String modelShortName = App.prop.getProperty("crop.shortname");
-			String fileA = App.prop.getProperty("fileA.location");
-			String fileT = App.prop.getProperty("fileT.location");
-			String fileXHead = App.prop.getProperty("fileX.head.location");
-			String fileXTail = App.prop.getProperty("fileX.tail.location");
-			String culHead = App.prop.getProperty("fileCUL.head.location");
-			String maxFiles = App.prop.getProperty("subfolders.number");
-			String vrname = App.prop.getProperty("CUL.varname");
-			String eco = App.prop.getProperty("CUL.ECO");
-			int n=Integer.parseInt(App.prop.getProperty("variable.number"));
-			String domi = "";
-			for(int i=0;i<n;) {
-				domi = App.prop.getProperty("variable"+ ++i);
-				String param[] = domi.split(" ");
-				domains.add(new CoefficientDomain(Double.parseDouble(param[0]), Double.parseDouble(param[1]), Double.parseDouble(param[2]), new DecimalFormat(param[3]), "%1$" + param[4] + "s"));
-			}
-			CropModel model = new CropModel(modelName, modelShortName, new File(fileA), new File(fileT), new File(fileXHead), new File(fileXTail), new File(culHead));
-			CropModelRun mr = new CropModelRun(domains, model, Integer.parseInt(maxFiles), vrname, eco, getTreatments(new File(fileXHead)));
-			ModelRunManager mrg = new ModelRunManager(mr);
-
-
-
-			return mrg;
-
-		
+		return mrg;
 
 	}
 
+	public enum readingStatus {
+		SEAK, READ, QUIT
+	};
 
-	public enum readingStatus{LOOK,READ,STOP};
-	
 	private List<Integer> getTreatments(File fileXHead) {
 		List<Integer> treatments = new ArrayList<Integer>();
 
-		Scanner reader;
-		String line="";
-		readingStatus flag = readingStatus.LOOK;
+		
+		String line = "";
+		readingStatus flag = readingStatus.SEAK;
 
 		if (fileXHead.exists()) {
 
-			try {
-				reader = new Scanner(fileXHead);
-				while (flag != readingStatus.STOP && reader.hasNextLine() ) {
+			try(Scanner reader=new Scanner(fileXHead)) {
+
+				while (flag != readingStatus.QUIT && reader.hasNextLine()) {
 					line = reader.nextLine();
 					if (line.contains("*TREATMENTS")) {
 						flag = readingStatus.READ;
 					}
 					if (line.contains("*CULTIVARS")) {
-						flag = readingStatus.STOP;
+						flag = readingStatus.QUIT;
 					}
-					if(flag==readingStatus.READ && !line.isEmpty() && !line.startsWith("@") && !line.startsWith("*")){
-						//TODO validate the treatment is from the cultivar ID=1
-						//TODO find a more elegant way to get the treatment ID 
-						treatments.add(Integer.parseInt(line.substring(0, 2).trim())); 
+					if (flag == readingStatus.READ && !line.isEmpty() && !line.startsWith("@") && !line.startsWith("*") && !line.startsWith("!")) {
+						// TODO validate the treatment is from the cultivar ID=1
+						// TODO find a more elegant way to get the treatment ID
+						treatments.add(Integer.parseInt(line.substring(0, 2).trim()));
 					}
 				}
-				reader.close();
+				
 			} catch (FileNotFoundException e) {
 				e.printStackTrace();
 			}
 
-			
-		}else{
+		} else {
 			treatments.add(1);
 		}
 		return treatments;
